e4273c2b823a4cb60348b8e6f17bf09eba37dd26
Marc Zwiers <mczwiers@gmail.com>
2022-09-13 17:22:50 +0200

    jjj

---
+++ src/components/WedstrijdCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card-wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: "WedstrijdCard",
+};
+</script>
+
+<style scoped>
+.card-wrapper {
+  margin: 0 auto;
+  margin-top: 30px;
+  display: flex;
+  width: 300px;
+  height: 250px;
+  background: rgb(255, 255, 255);
+  border: 2px solid orange;
+}
+</style>
