443e3e02aeb3ae07726c77122165d225fdf9a564
Marc Zwiers <mczwiers@gmail.com>
2022-12-07 13:58:34 +0100

    sadsad

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,44 @@
 <template>
-  <p>- Dit invullen</p>
+  <div class="roadmap-wrapper">
+    <h3>Algemeen</h3>
+    <ul>
+      <li>Eerste (visuele) bugs fixen</li>
+      <li>Alles goed leesbaar krijgen op verschillende devices</li>
+      <li>Goed logo fixen</li>
+      <li>Header herbouwen, neemt nu te veel ruimte in.</li>
+    </ul>
+    <h3>Home (deze pagina)</h3>
+    <ul>
+      <li>Nieuws tonen (nieuwe speler oid.)</li>
+      <li>Eerstvolgende wedstijd tonen</li>
+      <li>Aanwezigheid spelers</li>
+      <li>Een opstelling creator met de aangemelde spelers</li>
+    </ul>
+    <h3>Spelers</h3>
+    <ul>
+      <li>Betere fotos</li>
+      <li>Alles is net ff te klein of onleesbaar, dat fixen dus.</li>
+      <li>Header pas laten verschijnen als een spelers is aangeklikt</li>
+      <li>Fotos eventueel laten draaien als ze naar positie schuiven</li>
+      <li>Soepelere overgang om terug naar spelerovergang te gaan</li>
+    </ul>
+    <h3>Stats</h3>
+    <ul>
+      <li>Betere fotos</li>
+      <li>Alles is net ff te klein of onleesbaar, dat fixen dus.</li>
+      <li>Header pas laten verschijnen als een spelers is aangeklikt</li>
+      <li>Fotos eventueel laten draaien als ze naar positie schuiven</li>
+      <li>Soepelere overgang om terug naar spelerovergang te gaan</li>
+    </ul>
+    <h3>Spelers</h3>
+    <ul>
+      <li>Betere fotos</li>
+      <li>Alles is net ff te klein of onleesbaar, dat fixen dus.</li>
+      <li>Header pas laten verschijnen als een spelers is aangeklikt</li>
+      <li>Fotos eventueel laten draaien als ze naar positie schuiven</li>
+      <li>Soepelere overgang om terug naar spelerovergang te gaan</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -10,3 +49,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.roadmap-wrapper {
+  margin: 0 auto;
+  width: 300px;
+  text-align: left;
+  overflow-y: auto;
+}
+</style>
